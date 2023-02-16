e5782a3714ee2eb17053baa84a023fccb7e35d67
Parul Sharma <parulsharma@example.com>
2023-02-16 23:22:44 +0530

    01_variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+console.log("*****Step 1*******");
+var myfullName;
+console.log(myfullName);
+
+
+myfullName= "Parul Sharma";
+console.log(myfullName);
+
+console.log("*****Step 2*******");
+var myLove= "I love only JS";
+console.log(myLove);
+ 
+console.log("*****Step 3*******");
+var firstName="Parul";
+var lastName="Sharma";
+var age= "27";
+var address= "pune";
+var pincode="412207";
+var gender="female";
+var isMarried="yes";
+console.log("My  Name is",firstName,lastName,
+" Age:",age,
+"address:", address,
+"pin:",pincode,
+"gender:",gender,
+"married:",isMarried
+);
+
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,20 @@
-console.log (" Mohit Sharma ");
+console.log (" parul Sharma ");
+console.log("27");
+console.log("pune");
+
+
+var fullName= "parul Sharma";
+console.log(fullName);
+console.log(fullName);
+
+var city; // Variable Decleration
+console.log(city);
+city= "pune"; //variable initialization
+console.log(city);
+city= "surat"; //variable update
+console.log(city);
+
+var age=27; //variable Decleration and initiazation in same line
+age=28
+age=29
+console.log(age);
